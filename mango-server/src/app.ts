93bbd2865a9fdc74cadc34dfdb8406ac403e2a21
Yehor Levchenko <yehor.levchenko@nure.ua>
2026-04-02 16:59:31 +0300

    Added CORS

--- mango-server/src/app.ts
+++ mango-server/src/app.ts
@@ -3,9 +3,11 @@ import { profilesRouter } from "./profiles/profile.route"
 import { deviceModelsRouter } from "./device-models/device-model.route"
 import { devicesRouter } from "./devices/device.route"
 import { acousticMeasurementsRouter } from "./acoustic-measurements/acoustic-measurement.route"
+import { cors } from "hono/cors"
 
 export const app = new Hono()
 
+app.use(cors())
 app.route("/profiles", profilesRouter)
 app.route("/devices/models", deviceModelsRouter)
 app.route("/devices", devicesRouter)
